c0ea7da30bd7c1ae75ec032f41ec3133ddb3084b
ivnicolas <ikennanicolas@gmail.com>
2021-06-05 17:24:41 -0400

    commit tree cad0aadf89040c224a85e60cceb63423e13f2ea4

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your functions here
+function map(array,aFunction){
+   return  array.map(aFunction)
+}
+
+function reduce(array,aFunction,startingPoint){
+    if (startingPoint){
+        return array.reduce(aFunction,startingPoint)
+    }
+    else {
+        return array.reduce(aFunction)
+    }
+}
